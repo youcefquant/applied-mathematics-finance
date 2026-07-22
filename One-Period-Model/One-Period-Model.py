80f586654f36b5639e0004375fc025cdb1f52ba9
youcef_quant <youcefkouzi63@gmail.com>
2026-07-22 11:27:27 +0100

    Update One-Period-Model.py

--- One-Period-Model/One-Period-Model.py
+++ One-Period-Model/One-Period-Model.py
@@ -25,6 +25,32 @@ s2=(A_1*x)+(B_1*y)
 
 kV=(s2-s1)/s1
 
+import matplotlib.pyplot as plt
+
+apple_close = appl["Close"].iloc[:, 0]
+bound_close = bound["Close"].iloc[:, 0]
+
+portfolio_history = (apple_close * x) + (bound_close * y)
+plt.figure(figsize=(12, 6))
+plt.plot(portfolio_history,
+         label="Total Portfolio Value (V)",
+         color="#1f77b4",
+         linewidth=2.5)
+
+plt.title("Portfolio Value Growth Over Time ",
+          fontsize=14,
+          fontweight='bold',
+          pad=15)
+plt.xlabel("Date (التاريخ)",
+           fontsize=12)
+plt.ylabel("Total Value in USD ($ القيمة الإجمالية)",
+           fontsize=12)
+plt.grid(True,
+         linestyle="--",
+         alpha=0.6)
+plt.legend(fontsize=11)
+plt.show()
+
 print("Total portfolio before investment s1:",s1)
 print("Total portfolio after investment s2:",s2)
 print("Total returns :",kV)
